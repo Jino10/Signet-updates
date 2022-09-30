c916d6f79d6c4115b011ca0a7b1d32e5fbf529be
jino.x <jino.x@capestart.ngl>
2022-09-30 13:00:51 +0530

    user list page updated

--- src/Pages/Users/UsersList.js
+++ src/Pages/Users/UsersList.js
@@ -25,6 +25,12 @@ function UsersList({ userId }) {
   const [alertMessage, setAlertMessage] = useState('');
   const [show, setShow] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+  const [editId, setEditId] = useState('');
+  const [firstIndex, setFirstIndex] = useState(0);
+  const [lastIndex, setLastIndex] = useState(7);
+  const [updateData, setUpdateData] = useState({});
+  const [searchData, setSearchData] = useState('');
 
   const handleClose = () => setShow(false);
   const handleShow = (e) => {
@@ -43,11 +49,13 @@ function UsersList({ userId }) {
     }
     setUser(data.data);
   };
-  const handleClick = (e) => {
-    const { id } = e.target;
-    buttonTracker(gaEvents.NAVIGATE_EDIT_USER);
-    history(`/edituser/${id}`);
-  };
+  const handleClick = async (id) => {
+    setEditId(id);
+    setShowEdit(true);
+    const { 0: statusCode, 1: { data } } = await fetchCall(`${APIUrlConstants.GET_USER_DETAILS}/${id}`);
+    setUpdateData(data);
+  }
+
   const deleteUser = async () => {
     buttonTracker(gaEvents.DELETE_USER);
     setIsLoading(true);
@@ -86,58 +94,35 @@ function UsersList({ userId }) {
     }
     handleClose();
   };
-  const actionBtn = (_row, cell, _rowIndex) => (
-    <div className="actionBox d-flex align-items-center" data-testid="usertable">
-      <Button variant="link" id={cell.userId} onClick={handleClick}>
-        <img src={process.env.REACT_APP_PUBLIC_URL + 'images/users/edit.svg'} id={cell.userId} alt="Edit" />
-      </Button>
 
-      <Button
-        variant="link"
-        id={cell.userId}
-        status={cell.status}
-        onClick={handleShow}
-        disabled={cell.userId === localStorage.getItem('id') ? 'disabled' : ''}
-      >
-        <img src={process.env.REACT_APP_PUBLIC_URL + 'images/users/bin.svg'} id={cell.userId} status={cell.status} alt="Bin" />
-      </Button>
-    </div>
-  );
+  const updateUser = async () => {
+    buttonTracker(gaEvents.UPDATE_USER_DETAILS);
+    setIsLoading(true);
+    const { 0: status, 1: data } = await fetchCall(APIUrlConstants.UPDATE_USER_DETAILS, apiMethods.PUT, updateData);
+    const statusCode = status;
+    const responseData = data;
 
-  const fullName = (_cell, row, _rowIndex) => `${row.firstName} ${row.lastName}`;
-  function nameFilter(_cell, row) {
-    return `${row.firstName} ${row.lastName}`;
-  }
-  const columns = [
-    {
-      dataField: 'userId',
-      text: 'userId',
-      hidden: true,
-    },
-    {
-      dataField: 'fullName',
-      text: 'Name',
-      formatter: fullName,
-      filterValue: (cell, row) => nameFilter(cell, row),
-    },
-    {
-      dataField: 'orgEmail',
-      text: 'Email',
-    },
-    {
-      dataField: 'organization',
-      text: 'Organization',
-    },
-    {
-      dataField: 'status',
-      text: 'Status',
-    },
-    {
-      dataField: 'Action',
-      text: 'Action',
-      formatter: actionBtn,
-    },
-  ];
+    if (statusCode === httpStatusCode.SUCCESS) {
+      setShowAlert(true);
+      setAlertMessage('Updated successfully');
+      setError(false);
+      setShowEdit(false);
+      setTimeout(() => {
+        fetchAllUserDetails();
+        setTimeout(() => {
+          closeAlert();
+        }, 3000);
+      }, 2000);
+    } else {
+      setShowAlert(true);
+      setError(true);
+      setAlertMessage(responseData.message);
+      setIsLoading(false);
+      setTimeout(() => {
+        closeAlert();
+      }, 5000);
+    }
+  };
 
   useEffect(() => {
     fetchAllUserDetails();
@@ -150,34 +135,115 @@ function UsersList({ userId }) {
       <h6 className="text-center text-bold m-0 p-0">Fetching users ...</h6>
     );
 
+  const handleFirstName = (e) => {
+    setUpdateData({ ...updateData, firstName: e });
+  }
+
+  const handleLastName = (e) => {
+    setUpdateData({ ...updateData, lastName: e });
+  }
+
+  const handleStatus = (e) => {
+    setUpdateData({ ...updateData, status: e });
+  }
+
+  const data = users.slice(firstIndex, lastIndex);
+
+  const nextCall = () => {
+    setFirstIndex(firstIndex + 7);
+    setLastIndex(lastIndex + 7);
+  }
+
+  const previousCall = () => {
+    setFirstIndex(firstIndex - 7);
+    setLastIndex(lastIndex - 7);
+  }
+
   return (
     <div className="tabelBase" data-test-id="usertable">
       {isLoading && <Loading />}
-      <ToolkitProvider keyField="userId" data={users} columns={columns} search>
-        {(props) => (
-          <>
-            <div className="titleHeader d-flex align-items-center justify-content-between">
-              <div className="info">
-                <h6>Users</h6>
-              </div>
-              <div className="headerAction d-flex align-items-center">
-                <div className="searchWIC">
-                  <SearchBar {...props.searchProps} />
-                  <img className="inputIcon" src={process.env.REACT_APP_PUBLIC_URL + 'images/users/search.svg'} alt="inputIcon" />
-                </div>
-                <UserModal successCallback={fetchAllUserDetails} userId={userId} />
-              </div>
-            </div>
-            <div className="tableBaseBox">
-              <BootstrapTable
-                {...props.baseProps}
-                pagination={paginationFactory({ sizePerPage: 10 })}
-                noDataIndication={emptyDataMessage}
-              />
-            </div>
-          </>
-        )}
-      </ToolkitProvider>
+      <div className="titleHeader d-flex align-items-center justify-content-between">
+        <div className="info">
+          <h6>Users</h6>
+        </div>
+        <div className="headerAction d-flex align-items-center">
+          <div className="searchWIC">
+            <input type="text" className='form-control' value={searchData} onChange={(e) => setSearchData(e.target.value)} />
+            <img className="inputIcon" src={process.env.REACT_APP_PUBLIC_URL + 'images/users/search.svg'} alt="inputIcon" />
+          </div>
+          <UserModal successCallback={fetchAllUserDetails} userId={userId} />
+        </div>
+      </div>
+      <table className='table table-striped bordered'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Organization</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.filter((datas) => {
+              if (searchData === '') {
+                return datas;
+              }
+              if (datas.firstName.toLowerCase().includes(searchData.toLowerCase())) {
+                return datas;
+              }
+              return false;
+            })
+              .map((val) =>
+                <tr key={val.userId}>
+                  {showEdit && editId === val.userId ?
+                    <td>
+                      <div className='inputBox'>
+                        <input type="text" className='firstName' defaultValue={val.firstName} onChange={(e) => handleFirstName(e.target.value)} />
+                        <input type="text" defaultValue={val.lastName} onChange={(e) => handleLastName(e.target.value)} />
+                      </div>
+                    </td>
+                    :
+                    <td>{val.firstName}{val.lastName}</td>}
+                  <td>{val.orgEmail}</td>
+                  <td>{val.organization}</td>
+                  {showEdit && editId === val.userId ?
+                    <td>
+                      <select onChange={(e) => handleStatus(e.target.value)}>
+                        {val.status === "Pending" ? <option>Pending</option> : null}
+                        <option value="Active">Active</option>
+                        <option value="Inactive">Inactive</option>
+                      </select>
+                    </td>
+                    :
+                    <td>{val.status}</td>}
+                  <td>
+                    {showEdit && editId === val.userId ?
+                      (<span>
+                        <Button className='cancelBtn' onClick={() => setShowEdit(false)}>Cancel</Button>
+                        <Button className='saveBtn' onClick={updateUser}>Save</Button>
+                      </span>) :
+                      (<Button variant="link" id={val.userId} onClick={() => handleClick(val.userId)}>
+                        <img src={process.env.REACT_APP_PUBLIC_URL + 'images/users/edit.svg'} alt="Edit" />
+                      </Button>)
+                    }
+                    <Button
+                      variant="link"
+                      onClick={handleShow}
+                      disabled={val.userId === localStorage.getItem('id') ? 'disabled' : ''}
+                    >
+                      <img src={process.env.REACT_APP_PUBLIC_URL + 'images/users/bin.svg'} alt="Bin" />
+                    </Button>
+                  </td>
+                </tr>
+              )}
+        </tbody>
+        <div className='pageBtn'>
+          <span><i class="fa fa-angle-double-left" aria-hidden="true" /></span><Button className='previousBtn' onClick={previousCall}>Previous page</Button>
+          <Button className='nextBtn' onClick={nextCall}>Next page</Button><span><i class="fa fa-angle-double-right" aria-hidden="true" /></span>
+        </div>
+      </table>
 
       {showAlert && (
         <Alert variant={!error ? 'success' : 'danger'} className="alertWrapper" onClose={closeAlert} dismissible>
